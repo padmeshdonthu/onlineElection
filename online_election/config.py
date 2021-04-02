2c9a20db8427f0e489c4c7f377b08a22aebc1209
Padmesh Donthu <pd616769@dal.ca>
2021-04-02 17:36:29 -0300

    codebuild initial commit

--- online_election/config.py
+++ online_election/config.py
@@ -5,15 +5,16 @@ class ConfigParse:
 
     def fetch_keys(self):
         config = configparser.ConfigParser()
-        config.read('~/.aws/credentials')
+        config.read('.awskeys.ini')
         sections = config.sections()
-        if 'default' in sections:
+        if 'AWSEducateKeys' in sections:
             keys = {}
-            access_key_id = config['default']['aws_access_key_id']
-            secret_access_key = config['default']['aws_secret_access_key']
-            session_token = config['default']['aws_session_token']
+            access_key_id = config['AWSEducateKeys']['access_key_id']
+            secret_access_key = config['AWSEducateKeys']['secret_access_key']
+            session_token = config['AWSEducateKeys']['session_token']
+            region_name = config['AWSEducateKeys']['region_name']
             keys['access_key_id'] = access_key_id
             keys['secret_access_key'] = secret_access_key
             keys['session_token'] = session_token
-            keys['region_name'] = "us-east-1"
+            keys['region_name'] = region_name
             return keys
