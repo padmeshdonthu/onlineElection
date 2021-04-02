87af7b5f262c27373420d3cac109d66804b93cc4
padmeshdonthu <65316706+padmeshdonthu@users.noreply.github.com>
2021-04-01 21:07:41 -0300

    Update config.py

--- online_election/config.py
+++ online_election/config.py
@@ -9,9 +9,9 @@ class ConfigParse:
         sections = config.sections()
         if 'default' in sections:
             keys = {}
-            access_key_id = config['default']['access_key_id']
-            secret_access_key = config['default']['secret_access_key']
-            session_token = config['default']['session_token']
+            access_key_id = config['default']['aws_access_key_id']
+            secret_access_key = config['default']['aws_secret_access_key']
+            session_token = config['default']['aws_session_token']
             keys['access_key_id'] = access_key_id
             keys['secret_access_key'] = secret_access_key
             keys['session_token'] = session_token
